95ad5d4221a44bf2b341ecefb17d489039c27954
Peter Lohmann <peter@lohmann-online.info>
2016-07-03 19:55:22 +0200

    revert stupid idea

--- rottenpotatoes/features/step_definitions/web_steps.rb
+++ rottenpotatoes/features/step_definitions/web_steps.rb
@@ -90,13 +90,6 @@ When /^(?:|I )check "([^"]*)"$/ do |field|
   check(field)
 end
 
-# When /^I check ("[^"]*"(?:,\s*)?)+/ do |arg|
-#   fields = arg.scan(/"[^"]*"/).map {|str| str.sub('"', '')}
-#   fields.each {|field| check field}
-# end
-
-
-
 When /^(?:|I )uncheck "([^"]*)"$/ do |field|
   uncheck(field)
 end
@@ -233,7 +226,7 @@ Then /^the "([^"]*)" checkbox(?: within (.*))? should not be checked$/ do |label
     end
   end
 end
-
+ 
 Then /^(?:|I )should be on (.+)$/ do |page_name|
   current_path = URI.parse(current_url).path
   if current_path.respond_to? :should
@@ -247,8 +240,8 @@ Then /^(?:|I )should have the following query string:$/ do |expected_pairs|
   query = URI.parse(current_url).query
   actual_params = query ? CGI.parse(query) : {}
   expected_params = {}
-  expected_pairs.rows_hash.each_pair{|k,v| expected_params[k] = v.split(',')}
-
+  expected_pairs.rows_hash.each_pair{|k,v| expected_params[k] = v.split(',')} 
+  
   if actual_params.respond_to? :should
     actual_params.should == expected_params
   else
